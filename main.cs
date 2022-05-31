de97f1757aa76c765f4d528a4c2e0bd1a3522727
Cr8ted4GudWorks <65712694+Godwithus-Emma@users.noreply.github.com>
2022-05-31 15:36:29 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,24 @@
+using System;
+
+class Program {
+
+  
+  public static void Main (string[] args) {
+    
+    Console.WriteLine ("Hello World");
+
+    
+
+    int[] Sort(int[] s){
+      int[] sorted;
+      for(var i=0; i<s.Count; i++){
+        if(s[i] > s[i+1]){
+          sorted.
+        }
+      }
+      return sorted;
+    }
+    
+  }
+  
+}
